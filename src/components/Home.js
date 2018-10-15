224b119c743e1f91562b3dea8a36d646720a2cb1
Benjamin Austin <bnjmn80@gmail.com>
2018-10-14 21:23:18 -0600

    Added new bottled-lighnting photo for logo.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
 import '../App.css';
-import bottledLightning2 from '../images/bottledLightning2.png';
+import bottledLightning from '../images/bottledLightning4.png';
 import CFL from '../images/cFL.jpeg';
 import incandescent from '../images/incandescent.jpg';
 import tubeBulb from '../images/tubeBulb.jpeg';
@@ -13,15 +13,15 @@ const Home = (props) => {
     return (
         <div className="home">
             <div className="subHome">
-                <h1>ElectricFries</h1>
                 <Link to="/about" className="link incandescentLink">
                     <section className="linkSection">
                         <img src={incandescent} className="devices" alt="an incandescent bulb"/>
                         <h2 className="incandescent">About</h2>
                     </section>
                 </Link>
-                <section className="logoSection">
-                    <img src={bottledLightning2} className="devices" alt="lightning in a bottle" />
+                <section className="bottleLink">
+                    <img src={bottledLightning} className="power" alt="lightning in a bottle" />
+                    <h1 className="bottle">INFUSE</h1>
                 </section>
                 <Link to="/projects"  className="link CFLLink">
                     <section className="linkSection">
